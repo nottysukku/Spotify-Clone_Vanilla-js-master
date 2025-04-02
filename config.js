ea7669a43ae449901d068cddc2111a67560eab16
nottysukku <sukritchopra76@gmail.com>
2025-04-02 22:13:36 +0530

    changebackend

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const config = {
-    apiUrl: 'https://spotify-clone-backend-r672.onrender.com'
+    // apiUrl: 'https://spotify-clone-backend-r672.onrender.com'
+    apiUrl: 'https://new-folder-beryl-eta.vercel.app'
 };
